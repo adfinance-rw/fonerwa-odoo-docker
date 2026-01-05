7c701e174b143ae0243105a768b9333e2db12f91
Treasure <133379929+yvestresor@users.noreply.github.com>
2026-01-05 11:54:04 +0200

    Fix js issue of datepicker

--- custom_addons/hr_appraisal_objectives/static/src/js/custom_datepicker_limit.js
+++ custom_addons/hr_appraisal_objectives/static/src/js/custom_datepicker_limit.js
@@ -1,19 +1,36 @@
-odoo.define('your_module.custom_datepicker_limit', function (require) {
-    'use strict';
+/** @odoo-module **/
 
-    var FieldDate = require('web.basic_fields').FieldDate;
-    var field_registry = require('web.field_registry');
+import { registry } from "@web/core/registry";
 
-    var LimitedDatePicker = FieldDate.extend({
-        _renderEdit: function () {
-            this._super.apply(this, arguments);
+const fieldRegistry = registry.category("fields");
+// In Odoo 18 the registry entry is an object, e.g. { component: DateField, ... }
+const { component: BaseDateField } = fieldRegistry.get("date");
 
-            var today = moment().endOf('day');
-            if (this.$input) {
-                this.$input.attr('max', today.format('YYYY-MM-DD'));
-            }
-        },
-    });
+/**
+ * LimitedDateField
+ *
+ * Backend date widget that prevents selecting a date in the future by
+ * configuring the underlying datepicker `maxDate` option.
+ */
+export class LimitedDateField extends BaseDateField {
+    /**
+     * Extend the base datepicker options to set a `maxDate` equal to today.
+     * This is the modern equivalent of adding `max="YYYY-MM-DD"` on the input.
+     */
+    get datepickerOptions() {
+        const options = { ...super.datepickerOptions };
 
-    field_registry.add('limited_date', LimitedDatePicker);
-});
+        // Compute today's date (no time component) in the local timezone
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        // Flatpickr (used by Odoo) accepts a JS Date instance for maxDate
+        options.maxDate = today;
+
+        return options;
+    }
+}
+
+// Register the widget so it can be used with widget="limited_date"
+// In Odoo 18 the registry expects a config object: { component, props, ... }
+fieldRegistry.add("limited_date", { component: LimitedDateField });
